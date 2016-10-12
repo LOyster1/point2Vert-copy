16564624d5ee7f335d75dc9d151f9b55c6c25bfc
LOyster1 <loyster1@alaska.edu>
2016-10-12 15:38:19 -0800

    Spinning cube

---
+++ build/point2Vert.build/Debug/point2Vert.build/Objects-normal/x86_64/scgMPVAOs.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /College\ Semester\ Files/Fall\ 2016/Graphics/pointer2vertexMacOS/scgMPVAOs.cpp \
+  /College\ Semester\ Files/Fall\ 2016/Graphics/pointer2vertexMacOS/Angel.h \
+  /College\ Semester\ Files/Fall\ 2016/Graphics/pointer2vertexMacOS/vec.h \
+  /College\ Semester\ Files/Fall\ 2016/Graphics/pointer2vertexMacOS/mat.h \
+  /College\ Semester\ Files/Fall\ 2016/Graphics/pointer2vertexMacOS/CheckError.h
